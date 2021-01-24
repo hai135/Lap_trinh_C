7b62f1e7c903371d030599650b2bc1312bd9fad6
haiNguyenThanh13052001 <hai13052001@gmail.com>
2021-01-24 11:23:57 +0700

    update

--- Ham/bai5.c
+++ Ham/bai5.c
@@ -4,7 +4,7 @@ int main(){
     puts("Nhap 2 so la khoang a, b:");
     scanf("%d%d", &a, &b);
     srand(time(NULL));
-    int tmp = b- a;
+    int tmp = b - a;
     int x = (rand()%tmp)+a;
     printf("%d", x);
 }
